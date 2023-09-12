081c4821c27a38c02e701d46ad691478b8cc99a0
Tay Cheng <tay.cheng@sunbirddcim.com>
2023-09-12 12:22:30 +0800

    update

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,71 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
+import { CoursesService } from "./courses.service";
+
+describe("CoursesService", () => {
+  let coursesService: CoursesService, controller: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoursesService],
+    });
+
+    coursesService = TestBed.inject(CoursesService);
+    controller = TestBed.inject(HttpTestingController);
+  });
+
+  it("should retrieve all courses", () => {
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy("No courses returned");
+      expect(courses.length).toBe(12, "incorrect number of courses");
+
+      const course = courses.find((course) => course.id === 12);
+      expect(course.titles.description).toBe("Angular Testing Course");
+    });
+
+    const req = controller.expectOne("/api/courses");
+    expect(req.request.method).toEqual("GET");
+    req.flush({ payload: Object.values(COURSES) });
+  });
+
+  it("should find course by id", () => {
+    coursesService.findCourseById(12).subscribe((course) => {
+      expect(course).toBeTruthy("No courses returned");
+      expect(course.id).toBe(12);
+    });
+
+    const req = controller.expectOne("/api/courses/12");
+    expect(req.request.method).toEqual("GET");
+    req.flush(COURSES[12]);
+  });
+
+  it("should save course", () => {
+    const changes: Partial<Course> = {
+      titles: { description: "Testing Course" },
+    };
+
+    coursesService.saveCourse(12, changes).subscribe((course) => {
+      expect(course.id).toBe(12);
+    });
+
+    const req = controller.expectOne("/api/courses/12");
+    expect(req.request.method).toEqual("PUT");
+    expect(req.request.body.titles.description).toEqual(
+      changes.titles.description
+    );
+    req.flush({
+      ...COURSES[12],
+      ...changes,
+    });
+  });
+
+  afterEach(() => {
+    controller.verify();
+  });
+});
